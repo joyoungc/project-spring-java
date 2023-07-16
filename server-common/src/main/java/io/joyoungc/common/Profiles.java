06d67b1a9d0f6304676ecf8b7a61d02cd0e754ca
joyoungc <aiden.jeong.jy@gmail.com>
2023-07-16 18:48:23 +0900

    Add Profiles class

---
+++ server-common/src/main/java/io/joyoungc/common/Profiles.java
@@ -0,0 +1,11 @@
+package io.joyoungc.common;
+
+public final class Profiles {
+
+    private Profiles() {}
+
+    public static final String ALPHA = "alpha";
+    public static final String BETA = "beta";
+    public static final String TESTCONTAINERS = "tc";
+    public static final String TEST = "test";
+}
