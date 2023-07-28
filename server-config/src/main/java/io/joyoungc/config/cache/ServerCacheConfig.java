62fecc0a00b0e16cc9ca62f7486596c184f43f73
joyoungc <aiden.jeong.jy@gmail.com>
2023-07-28 22:45:37 +0900

    Polish settings related to redis

--- server-config/src/main/java/io/joyoungc/config/cache/ServerCacheConfig.java
+++ server-config/src/main/java/io/joyoungc/config/cache/ServerCacheConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -19,41 +20,42 @@ import java.time.Duration;
  */
 @Configuration
 @EnableCaching
+@Profile("redis")
 public class ServerCacheConfig {
 
     public static final String CACHE_JDK_MANAGER = "redisCacheJdkManager";
 
-    /*
+    /**
      * redis cache (json)
      *
      * @param connectionFactory
      * @return
      */
-//    @Primary
-//    @Bean
-//    public CacheManager cacheManager(LettuceConnectionFactory connectionFactory) {
-//        RedisCacheConfiguration redisConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-//                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-//                .disableCachingNullValues()
-//                .entryTtl(Duration.ofMinutes(1));
-//        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
-//                .cacheDefaults(redisConfiguration).build();
-//    }
+    @Primary
+    @Bean
+    public CacheManager cacheManager(LettuceConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redisConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofMinutes(1));
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
+                .cacheDefaults(redisConfiguration).build();
+    }
 
-    /*
+    /**
      * redis cache (jdk)
      *
      * @param connectionFactory
      * @return
      */
-//    @Bean
-//    public CacheManager redisCacheJdkManager(LettuceConnectionFactory connectionFactory) {
-//        RedisCacheConfiguration redisConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-//                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-//                .disableCachingNullValues()
-//                .entryTtl(Duration.ofMinutes(1));
-//        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
-//                .cacheDefaults(redisConfiguration).build();
-//    }
+    @Bean
+    public CacheManager redisCacheJdkManager(LettuceConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redisConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofMinutes(1));
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
+                .cacheDefaults(redisConfiguration).build();
+    }
 }
