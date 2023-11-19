8db7282e8f1ea73f709273ca9e83b4ed50b6127b
joyoungc <aiden.jeong.jy@gmail.com>
2023-11-19 18:43:06 +0900

    Add a Profiles class

---
+++ server-infrastructure/src/main/java/io/joyoungc/infrastructure/constant/Profiles.java
@@ -0,0 +1,16 @@
+package io.joyoungc.infrastructure.constant;
+
+public final class Profiles {
+
+    private Profiles() {
+    }
+
+    public static final String TEST = "test";
+    public static final String LOCAL = "local";
+    public static final String ALPHA = "alpha";
+    public static final String BETA = "beta";
+    public static final String PRODUCTION = "prod";
+
+    public static final String REDIS = "redis";
+    public static final String MONGO = "mongo";
+}
