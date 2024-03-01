763f6ef84c01333b8d2f6785fce9f30f445e58ce
joyoungc <aiden.jeong.jy@gmail.com>
2024-03-02 00:24:06 +0900

    Refactor test codes

---
+++ server-infrastructure/src/test/java/io/joyoungc/infrastructure/TestServerInfrastructureApplication.java
@@ -0,0 +1,7 @@
+package io.joyoungc.infrastructure;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServerInfrastructureApplication {
+}
