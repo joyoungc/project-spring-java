4088b9fb8852e83288ebe313c11196443d324c0e
joyoungc <aiden.jeong.jy@gmail.com>
2024-06-02 00:39:21 +0900

    Rename the PK column in tables

--- server-infrastructure/src/testFixtures/resources/sql/schema.sql
+++ server-infrastructure/src/testFixtures/resources/sql/schema.sql
@@ -1,46 +1,46 @@
 CREATE TABLE member
 (
-    created_date  DATETIME,
-    member_id     BIGINT AUTO_INCREMENT,
-    modified_date DATETIME,
-    city          VARCHAR(255),
-    created_by    VARCHAR(255),
-    grade         VARCHAR(255) COMMENT '[VIP,BASIC]',
-    modified_by   VARCHAR(255),
+    id     BIGINT AUTO_INCREMENT,
     name          VARCHAR(255) not null,
+    grade         VARCHAR(255) COMMENT '[VIP,BASIC]',
+    city          VARCHAR(255),
     street        VARCHAR(255),
     zip_code      VARCHAR(255),
-    PRIMARY KEY (member_id)
+    created_by    VARCHAR(255),
+    created_date  DATETIME COMMENT '생성일',
+    modified_by   VARCHAR(255),
+    modified_date DATETIME COMMENT '수정일',
+    PRIMARY KEY (id)
 );
 
 
 CREATE TABLE orders
 (
-    created_date   DATETIME,
+    id       BIGINT AUTO_INCREMENT,
     discount_price BIGINT,
-    member_id      BIGINT,
-    modified_date  DATETIME,
     order_date     DATETIME,
-    order_id       BIGINT AUTO_INCREMENT,
+    status         VARCHAR(255) comment '[ORDER,CANCEL]',
+    member_id      BIGINT,
     product_id     BIGINT UNIQUE,
     created_by     VARCHAR(255),
+    created_date   DATETIME,
     modified_by    VARCHAR(255),
-    status         VARCHAR(255) comment '[ORDER,CANCEL]',
-    PRIMARY KEY (order_id)
+    modified_date  DATETIME,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE product
 (
+    id    BIGINT AUTO_INCREMENT,
     created_date  DATETIME,
     modified_date DATETIME,
     price         BIGINT,
-    product_id    BIGINT AUTO_INCREMENT,
     created_by    VARCHAR(255),
     modified_by   VARCHAR(255),
     name          VARCHAR(255),
-    PRIMARY KEY (product_id)
+    PRIMARY KEY (id)
 );
 
-ALTER TABLE orders ADD CONSTRAINT fk_orders_01 FOREIGN KEY (member_id) REFERENCES member (member_id);
+ALTER TABLE orders ADD CONSTRAINT fk_orders_01 FOREIGN KEY (member_id) REFERENCES member (id);
 
-ALTER TABLE orders ADD CONSTRAINT fk_orders_02 FOREIGN KEY (product_id) REFERENCES product (product_id);
+ALTER TABLE orders ADD CONSTRAINT fk_orders_02 FOREIGN KEY (product_id) REFERENCES product (id);
